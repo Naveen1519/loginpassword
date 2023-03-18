00f59e36e4d6d59a1e2bc87d55a989110cb66523
Naveen1519 <naveen150999@gmail.com>
2023-03-18 16:36:13 +0530

    1project
    login and password using java+jdbc+servlet

---
+++ Login1/src/cscorner/Login1Servlet.java
@@ -0,0 +1,67 @@
+package cscorner;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.mysql.cj.protocol.Resultset;
+
+import jdk.nashorn.internal.ir.ForNode;
+
+/**
+ * Servlet implementation class Login1Servlet
+ */
+@WebServlet("/Login1Servlet")
+public class Login1Servlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+ 
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		try {
+			PrintWriter out=response.getWriter();
+			response.setContentType("text/html");
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/naveen","root","admin");
+
+		String n=request.getParameter("txtName");
+		String p=request.getParameter("txtpwd");
+		PreparedStatement ps=con.prepareStatement("select uname from login where uname=? and password=?");
+		ps.setString(1, n);
+		ps.setString(2, p);
+		 ResultSet rs= ps.executeQuery();
+		 if(rs.next()) {
+			 RequestDispatcher rd=request.getRequestDispatcher("welcome.jsp");
+			 rd.forward(request, response);
+		 }
+		 else {
+			 
+			 out.println("<font color=red size=18>login1 Failed!!<br>");
+			 out.println("<a.href=login.jsp>Try Again!!</a>");
+			 
+		 }
+		 
+		}
+		catch(ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+			catch(SQLException e) {
+				e.printStackTrace();
+			};
+			
+		
+	}
+
+}
